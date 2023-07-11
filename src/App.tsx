0cf7ec99a365c6941690f434c2aadb4259849971
Iamswart <swartjide@gmail.com>
2023-07-11 03:33:35 +0100

    Added Spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,49 @@
+import { useState, useEffect } from "react";
 import About from "./components/About";
 import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import Project from "./components/Project";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import LoadingScreen from "./components/LoadingScreen";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setIsLoading(false);
+  //   }, 3500);
+
+  //   return () => clearTimeout(timer);
+  // }, []);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      setIsLoading(false);
+    };
+
+    window.addEventListener("load", handleLoad);
+
+    return () => {
+      window.removeEventListener("load", handleLoad);
+    };
+  }, []);
+
   return (
     <>
-      <NavBar />
-      <Hero />
-      <About />
-      <Project />
-      <Contact />
-      <Footer />
+      {isLoading ? (
+        <LoadingScreen />
+      ) : (
+        <>
+          <NavBar />
+          <Hero />
+          <About />
+          <Project />
+          <Contact />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function LoadingScreen() {
+  return (
+    <div className="loading">
+      <img
+        src="https://res.cloudinary.com/dfscst5lw/image/upload/v1689033704/portfolio_website/swartjide-website-favicon-color_ckh0fb.png"
+        alt="Loading"
+      />
+    </div>
+  );
+}
+
+export default LoadingScreen;
