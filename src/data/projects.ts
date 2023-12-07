7e2a8617d8fb598d98318d57175e4d0f7b729618
Iamswart <swartjide@gmail.com>
2023-12-07 16:54:14 +0100

    added directcash

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,11 +6,9 @@ export default [
     imageUrl:
       "https://res.cloudinary.com/dfscst5lw/image/upload/v1688980885/portfolio_website/game_yzyuyb.png",
     skills: ["HTML", "CSS", "JavaScript", "GIT", "Php"],
-    // heroTitle: "This page contains the case study of Dopefolio Open-Source Project which includes the Project Overview, Tools Used and Live Links to the official product.",
+
     summary:
       "Game Hub is a video game discovery web app that helps you find new and interesting games to play. With GameHub, you can search for games by platform, genre, and more.",
-    // description:
-    //   "Dopefolio is an Open-Source project which is a simple and clean multi-page portfolio website template for developers. I created this project for developers to quickly build a good-looking and fast-performing multi-page portfolio without having to code their portfolio from scratch. Since the launch of this project, It has received more than 2k stars on Github and it has also got featured on hundreds of sites and CSS-tricks.com has also featured this template as the hottest frontend tool of 2021 in one of their articles which you can find here. Dopefolio is now being used by thousands of developers globally and it has helped many people in landing jobs & opportunities which makes me happy that my creation is helping other people in building their careers. It has many other features like Dopefolio's repo contains a playground link which people can use to test the template with different theme colours to find their own preferred primary colour for the template. Feel free to check out the Project by visiting the Project Link.",
   },
   {
     id: 2,
@@ -19,23 +17,30 @@ export default [
     imageUrl:
       "https://res.cloudinary.com/dfscst5lw/image/upload/v1688980888/portfolio_website/folham_kqs72y.png",
     skills: ["HTML", "CSS", "JavaScript", "GIT", "Php"],
-    // heroTitle: "This page contains the case study of Dopefolio Open-Source Project which includes the Project Overview, Tools Used and Live Links to the official product.",
+
     summary:
       "A web app for Folham, an outdoor advertising company that revolutionizes the way businesses find prime advertising spaces. Users can easily browse and request quotes for billboards, digital displays, and more. Simplify your advertising campaigns with Folham user-friendly platform.",
-    // description:
-    //   "Dopefolio is an Open-Source project which is a simple and clean multi-page portfolio website template for developers. I created this project for developers to quickly build a good-looking and fast-performing multi-page portfolio without having to code their portfolio from scratch. Since the launch of this project, It has received more than 2k stars on Github and it has also got featured on hundreds of sites and CSS-tricks.com has also featured this template as the hottest frontend tool of 2021 in one of their articles which you can find here. Dopefolio is now being used by thousands of developers globally and it has helped many people in landing jobs & opportunities which makes me happy that my creation is helping other people in building their careers. It has many other features like Dopefolio's repo contains a playground link which people can use to test the template with different theme colours to find their own preferred primary colour for the template. Feel free to check out the Project by visiting the Project Link.",
   },
   {
     id: 3,
+    title: "DirectCash",
+    link: "https://www.dyrectpay.com/",
+    imageUrl:
+      "https://res.cloudinary.com/dfscst5lw/image/upload/v1701963531/portfolio_website/directpay_jppmkd.png",
+    skills: ["HTML", "CSS", "TypeScript", "GIT", "Php"],
+
+    summary:
+      "DirectCash simplifies the conversion of digital assets. It offers a user-friendly platform for quick and secure transactions, catering to various digital currencies and tokens. The site combines ease of use with robust security, making digital asset conversion effortless and reliable.",
+  },
+  {
+    id: 4,
     title: "St.Agnes Catholic Church",
     link: "http://saintagnesmaryland.org/",
     imageUrl:
       "https://res.cloudinary.com/dfscst5lw/image/upload/v1689347768/portfolio_website/agnes_pgrxyq.png",
     skills: ["HTML", "CSS", "JavaScript", "GIT", "Php"],
-    // heroTitle: "This page contains the case study of Dopefolio Open-Source Project which includes the Project Overview, Tools Used and Live Links to the official product.",
+
     summary:
       "I created a user-friendly website for St.Agnes Catholic church, a church located in Lagos, NG. It enables seamless streaming of their Mass services",
-    // description:
-    //   "Dopefolio is an Open-Source project which is a simple and clean multi-page portfolio website template for developers. I created this project for developers to quickly build a good-looking and fast-performing multi-page portfolio without having to code their portfolio from scratch. Since the launch of this project, It has received more than 2k stars on Github and it has also got featured on hundreds of sites and CSS-tricks.com has also featured this template as the hottest frontend tool of 2021 in one of their articles which you can find here. Dopefolio is now being used by thousands of developers globally and it has helped many people in landing jobs & opportunities which makes me happy that my creation is helping other people in building their careers. It has many other features like Dopefolio's repo contains a playground link which people can use to test the template with different theme colours to find their own preferred primary colour for the template. Feel free to check out the Project by visiting the Project Link.",
   },
 ];
